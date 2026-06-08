56520016f2b8c676c57c919b2e66ccaf67230a59
d7representacao-hub <d7representacao@gmail.com>
2026-06-07 22:39:28 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,17 @@
 
+const CACHE_NAME = 'd7hub-v3';
+const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-152.png'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(cached => cached || fetch(e.request)));
+});
