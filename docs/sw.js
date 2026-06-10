4d5d8e04a1265be7ea869c53e7e81dd67324c44a
d7representacao-hub <d7representacao@gmail.com>
2026-06-10 16:02:18 -0300

    Add files via upload

---
+++ docs/sw.js
@@ -0,0 +1,19 @@
+const CACHE_NAME = 'd7hub-v78';
+const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-152.png'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.map(k => caches.delete(k))))
+      .then(() => caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
